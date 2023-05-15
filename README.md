edd55e887a1560cec505b91af4ac41c3f0326239
bababum <bababum1995@gmail.com>
2023-05-15 19:22:26 +0700

    readme

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-Путешествие по России
-Проектная работа Яндекс Практикума №3. Одностраничный сайт. Использовали CSS и HTML.
-https://bababum95.github.io/russian-travel/
+# Project Russian travel
+
+Adaptive website using HTML, CSS and JS with modal windows
+
+## Description
+
+This project is an adaptive website created using HTML, CSS and JS. It features a responsive design that works well on a variety of devices, from desktops to smartphones. The website also includes modal windows, which are created using JavaScript.
+
+The website has been built using the BEM methodology, which stands for Block, Element, Modifier. BEM is a naming convention for CSS classes that makes it easier to maintain and update the codebase.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Features
+
+- Responsive design
+- Modal windows
+- BEM methodology
+- Cross-browser compatibility
+
+## Usage
+
+The web page is available for viewing in any web browser.
+[ссылка на GitHub Pages](https://bababum95.github.io/russian-travel/) 
+
+## Credits
+
+The project was completed as a part of the Yandex.Practicum curriculum.
--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,37 @@
-const popupContainer = document.querySelector('.popup')
-const btnAboutProject = document.querySelector('.header__about-project')
-const btnClose = popupContainer.querySelector('.popup__close')
-const btnOpenImage = popupContainer.querySelector('.popup__btn-image-open')
-const image = popupContainer.querySelector('.popup__image')
-const btnCloseImage = image.querySelector('.popup__image-close')
+const popupContainer = document.querySelector('.popup');
+const btnAboutProject = document.querySelector('.header__about-project');
+const btnOpenImage = popupContainer.querySelector('.popup__btn-image-open');
+const image = popupContainer.querySelector('.popup__image');
 
-
-btnAboutProject.addEventListener('click', openPopup)
-btnClose.addEventListener('click', closePopup)
-btnOpenImage.addEventListener('click', OpenImage)
-btnCloseImage.addEventListener('click', closeImage)
-
-function openPopup() {
-    popupContainer.classList.add('popup_opened')
-}
+function openPopup(evt) {
+    evt.preventDefault();
+    popupContainer.classList.add('popup_opened');
+};
 function closePopup() {
-    popupContainer.classList.remove('popup_opened')
-    closeImage()
-}
+    popupContainer.classList.remove('popup_opened');
+    closeImage();
+};
 function closeImage() {
-    btnOpenImage.classList.remove('popup__hide-object')
-    image.classList.add('popup__hide-object')
-}
-function OpenImage() {
-    btnOpenImage.classList.add('popup__hide-object')
-    image.classList.remove('popup__hide-object')
-}
+    btnOpenImage.classList.remove('popup__hide-object');
+    image.classList.add('popup__hide-object');
+};
+function OpenImage(evt) {
+    evt.preventDefault();
+    btnOpenImage.classList.add('popup__hide-object');
+    image.classList.remove('popup__hide-object');
+};
+
+btnAboutProject.addEventListener('click', openPopup);
+btnOpenImage.addEventListener('click', OpenImage);
+
+popupContainer.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+        closePopup();
+    };
+    if (evt.target.classList.contains('popup__close')) {
+        closePopup();
+    };
+    if (evt.target.classList.contains('popup__image-close')) {
+        closeImage();
+    };
+});
